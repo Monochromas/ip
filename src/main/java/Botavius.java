3d93123378c09054d586a87893dc13e592bd4b57
Samuel Chew <73582380+Monochromas@users.noreply.github.com>
2026-08-21 16:16:18 +0800

    Add greet and goodbye functions

--- src/main/java/Botavius.java
+++ src/main/java/Botavius.java
@@ -1,15 +1,47 @@
 import java.util.Scanner;
 
+/**
+ * Runs the Botavius command-line application.
+ */
 public class Botavius {
 
+    /**
+     * Starts the application, displays its banner, and shows the greeting
+     * and goodbye messages.
+     *
+     * @param args command-line arguments, which are not used
+     */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         String banner = " ____   ___ _____  _  __     _____ _   _ ____  \n"
                 + "| __ ) / _ \\_   _|/ \\ \\ \\   / /|_ _| | | / ___|\n"
                 + "|  _ \\| | | || | / _ \\ \\ \\ / /  | || | | \\___ \\\n"
                 + "| |_) | |_| || |/ ___ \\ \\ v /   | || |_| |___) |\n"
                 + "|____/ \\___/ |_/_/   \\_\\ \\_/   |____\\___/|____/\n";
         System.out.println(banner);
+        greet();
+        goodbye();
     }
-    
+
+    /**
+     * Prints the application's greeting message.
+     */
+    public static void greet() {
+        String greeting = """        
+        Hello! I'm Botavius: \n
+        What can I do for you?\n
+        ____________________________________________________________
+        """;
+        System.out.println(greeting);
+    }
+
+    /**
+     * Prints the application's goodbye message.
+     */
+    public static void goodbye() {
+        String goodbye = """
+        Bye. Hope to see you again soon!\n
+        ____________________________________________________________
+        """;
+        System.out.println(goodbye);
+    }
 }
