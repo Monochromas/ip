ce6e9ca0c96d062c9beb03ae67da423965360fc7
Samuel Chew <73582380+Monochromas@users.noreply.github.com>
2026-08-21 16:10:15 +0800

    Change name to botavius and refactor references to duke

--- src/main/java/Botavius.java
+++ src/main/java/Botavius.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
 public class Botavius {
-    public static String[] stored_commands = new String[100];
-    public static int index = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String banner = " ____   ___ _____  _  __     _____ _   _ ____  \n"
@@ -12,4 +11,5 @@ public class Botavius {
                 + "|____/ \\___/ |_/_/   \\_\\ \\_/   |____\\___/|____/\n";
         System.out.println(banner);
     }
+    
 }
