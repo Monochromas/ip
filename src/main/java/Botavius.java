6f904b9f1571c4eec05cabcc43e42d245359bd9a
Samuel Chew <73582380+Monochromas@users.noreply.github.com>
2026-08-21 14:31:39 +0800

    Level 0

--- src/main/java/Botavius.java
+++ src/main/java/Botavius.java
@@ -6,5 +6,25 @@ public class Botavius {
                 + "| |_) | |_| || |/ ___ \\ \\ v /   | || |_| |___) |\n"
                 + "|____/ \\___/ |_/_/   \\_\\ \\_/   |____\\___/|____/\n";
         System.out.println(banner);
+        greet();
+        goodbye();
+
+    }
+
+    public static void greet() {
+        String greeting = """        
+        Hello! I'm Botavius: \n
+        What can I do for you?\n
+        ____________________________________________________________
+        """;
+        System.out.println(greeting);
+    }
+
+    public static void goodbye() {
+        String goodbye = """
+        Bye. Hope to see you again soon!\n
+        ____________________________________________________________
+        """;
+        System.out.println(goodbye);
     }
 }
